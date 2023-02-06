b3bb0484c55e584eeab5e8993dd4f28364ba5481
dclemon <63597032+dclemon@users.noreply.github.com>
2023-02-06 21:06:32 +0800

    Update yimu.js

--- yimu.js
+++ yimu.js
@@ -1,6 +1,6 @@
 
 /*  
-^https:\/\/yimuapp.com:8082\/bookkeeping\/user\/(getUser) url script-response-body https://raw.githubusercontent.com/dclemon/lemonScripts/main/scripts/yimu.js?token=GHSAT0AAAAAAB5FM3KDLBYV6U37CF67ZED2Y7A7L3A
+^https:\/\/yimuapp.com:8082\/bookkeeping\/user\/(getUser) url script-response-body https://raw.githubusercontent.com/dclemon/pear/main/yimu.js
 MITM = yimuapp.com
 */
 var body = $response.body;
